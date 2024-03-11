107ad529e23d39628641b0174c814a171e51dce1
edsonariel <developeritl@itl3pl.com>
2024-03-11 15:21:18 -0600

    agregando plugin spring security y su configuracion

---
+++ src/main/java/com/edsonsarmiento/apiprueba/security/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.edsonsarmiento.apiprueba.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/api/prueba/").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails userDetails = User.builder()
+                .username("edson")
+                .password(passwordEncoder().encode("ariel"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
